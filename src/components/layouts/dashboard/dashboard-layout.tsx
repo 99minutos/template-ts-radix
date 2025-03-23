094230e0b1ae1b330273f8ddb9361ffa4756e170
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2025-03-22 20:40:40 -0700

    fix: custom-actions portal

--- src/components/layouts/dashboard/dashboard-layout.tsx
+++ src/components/layouts/dashboard/dashboard-layout.tsx
@@ -34,8 +34,8 @@ export function DashboardLayout(props: PropsWithChildren) {
     <div className="min-h-screen flex flex-col">
       {/* Mobile sidebar overlay */}
 
-      <header className="w-full g-white border-b h-16 border-b-secondaryborder-secondary">
-        <div className="h-full flex items-center justify-between px-4" aria-label="Global">
+      <header className="w-full g-white border-b h-16 border-b-gray-400">
+        <div className="h-full flex items-center justify-between px-4 gap-4" aria-label="Global">
           <div className="flex items-center">
             <div className={'flex-shrink-0 inline-flex items-center justify-center'}>
               <IconButton variant="ghost" onClick={onToggleSidebar}>
@@ -47,14 +47,14 @@ export function DashboardLayout(props: PropsWithChildren) {
               </a>
             </div>
           </div>
-          <div className="flex items-center gap-x-12" id="portal-actions">
+          <div className="flex-grow flex items-center gap-x-12" id="portal-actions">
             {/* Portal custom actions */}
           </div>
           <div className="flex items-center gap-3">
             <span className="select-none">{user?.name || ''}</span>
             <DropdownMenu.Root>
               <DropdownMenu.Trigger className="cursor-pointer">
-                <Avatar.Root className="inline-flex items-center justify-center size-12 rounded-full border border-gray-200 bg-secondary text-white font-bold text-lg">
+                <Avatar.Root className="inline-flex items-center justify-center size-12 rounded-full bg-secondary text-white font-bold text-lg">
                   <Avatar.Fallback>{stringAvatar(user?.name || '')}</Avatar.Fallback>
                 </Avatar.Root>
               </DropdownMenu.Trigger>
@@ -84,7 +84,7 @@ export function DashboardLayout(props: PropsWithChildren) {
           )}
         >
           <div className="flex flex-col h-full">
-            <div className="flex-grow overflow-y-auto px-2 space-y-1 bg-white border-r border-r-secondaryborder-secondary">
+            <div className="flex-grow overflow-y-auto px-2 space-y-1 bg-white border-r border-r-gray-400">
               <nav className="space-y-1 pt-2 relative">
                 {navigation.map((item) => (
                   <a
@@ -100,7 +100,7 @@ export function DashboardLayout(props: PropsWithChildren) {
                     <item.icon
                       className={`
                     mr-4 flex-shrink-0 h-6 w-6
-                    ${item.current ? 'text-white' : 'text-gray-400 group-hover:text-white'}
+                    ${item.current ? 'text-white' : 'text-secondary group-hover:text-white'}
                   `}
                     />
                     {item.name}
