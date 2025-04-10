0041eec0f4bc1937a69be26575654e926cfccec9
Carlos Rafael Arreola Hernández <rafael-arreola@outlook.com>
2025-04-10 10:38:55 -0700

    feat: upgrade template dashboard template

--- src/main.tsx
+++ src/main.tsx
@@ -15,11 +15,11 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Theme
-        radius="full"
+        //radius="full"
         accentColor="lime"
         grayColor="slate"
         panelBackground="solid"
-        scaling="110%"
+        //scaling="110%"
       >
         <AuthProvider>
           <RouteManager />
